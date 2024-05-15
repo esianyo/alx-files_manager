1ae1ffd4a60c96ffa39c0ee22d6e51fca816abcc
Constance Amatey <consamate57@gmail.com>
2024-05-15 20:16:24 +0000

    task 1

---
+++ utils/db.js
@@ -0,0 +1,56 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    this.client = new MongoClient (mongodb://${host}:${port}, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    this.connect();
+    this.db = this.client.db(database);
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const collection = this.db.collection('users');
+      const count = await collection.countDocuments();
+      return count;
+    } catch (error) {
+      console.error('Error getting the number of users:', error);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const collection = this.db.collection('files');
+      const count = await collection.countDocuments();
+      return count;
+    } catch (error) {
+      console.error('Error getting the number of files:', error);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
