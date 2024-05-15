545b19c6ed82d16ef14065b219f223439f73b545
Constance Amatey <consamate57@gmail.com>
2024-05-15 23:49:23 +0000

    files update

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,93 +1,25 @@
-#!/usr/bin/node
-
-const { v4 } = require('uuid');
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
-const { getAuthzHeader, getToken, pwdHashed } = require('../utils/utils');
-const { decodeToken, getCredentials } = require('../utils/utils');
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import { checkAuthReturnKey, findUserByCreds, credsFromBasicAuth } from '../utils/helpers';
 
 class AuthController {
-  static async getConnect(req, res) {
-    const authzHeader = getAuthzHeader(req);
-    if (!authzHeader) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const token = getToken(authzHeader);
-    if (!token) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const decodedToken = decodeToken(token);
-    if (!decodedToken) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const { email, password } = getCredentials(decodedToken);
-    const user = await dbClient.getUser(email);
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    if (user.password !== pwdHashed(password)) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const accessToken = v4();
-    await redisClient.set(`auth_${accessToken}`, user._id.toString('utf8'), 60 * 60 * 24);
-    res.json({ token: accessToken });
-    res.end();
+  static async getConnect(request, response) {
+    const creds = await credsFromBasicAuth(request);
+    if (!creds) return response.status(401).json({ error: 'Unauthorized' });
+    const user = await findUserByCreds(creds.email, creds.password);
+    if (!user) return response.status(401).json({ error: 'Unauthorized' });
+    const toke = uuidv4();
+    const k = `auth_${toke}`;
+    await redisClient.set(k, user._id.toString(), 60 * 60 * 24);
+    return response.json({ toke });
   }
 
-  static async getDisconnect(req, res) {
-    const token = req.headers['x-token'];
-    if (!token) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const id = await redisClient.get(`auth_${token}`);
-    if (!id) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const user = await dbClient.getUserById(id);
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    await redisClient.del(`auth_${token}`);
-    res.status(204).end();
-  }
-
-  static async getMe(req, res) {
-    const token = req.headers['x-token'];
-    if (!token) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const id = await redisClient.get(`auth_${token}`);
-    if (!id) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    const user = await dbClient.getUserById(id);
-    if (!user) {
-      res.status(401).json({ error: 'Unauthorized' });
-      res.end();
-      return;
-    }
-    res.json({ id: user._id, email: user.email }).end();
+  static async getDisconnect(request, response) {
+    const k = await checkAuthReturnKey(request);
+    if (!k) return response.status(401).json({ error: 'Unauthorized' });
+    await redisClient.del(k);
+    return response.status(204).end();
   }
 }
 
-module.exports = AuthController;
+export default AuthController;
--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-#!/usr/bin/node
+import express from 'express';
+import indexRouter from './routes/index';
 
-const express = require("express");
-const router = require("./routes/index");
+const app = express();
 
-const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+const port = process.env.PORT || 5000;
 
-server.use(express.json());
-server.use(router);
+app.use(express.json());
+app.use('/', indexRouter);
+app.listen(port, console.log(`Server running on port ${port}`));
 
-server.listen(PORT, () =>
-  console.log(`The server is running on port: ${PORT}`)
-);
+export default app;
